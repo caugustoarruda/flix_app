7f8356566db619c50b13a5ad2c9e976ba41eabc2
Carlos Arruda <caugustogarruda@gmail.com>
2026-05-08 17:13:39 -0300

    Sidebar menu

--- app.py
+++ app.py
@@ -1,6 +1,25 @@
 import streamlit as st
 
 
-st.title('Flix app Teste')
-st.text_input('Nome do filme')
-st.button('Salvar', type='tertiary')
+def main():
+    st.title('Flix app')
+    
+    menu_option = st.sidebar.selectbox(
+        'Selecione uma opção: ',
+        ['Início', 'Generos', 'Atores', 'Filmes', 'Avaliações']
+    )
+
+    if menu_option == 'Início':
+        st.write('Inicio')
+    elif menu_option == 'Generos':
+        st.write('Lista de generos')
+    elif menu_option == 'Atores':
+        st.write('Lista de Atores')
+    elif menu_option == 'Filmes':
+        st.write('Lista de Filmes')
+    else:
+        st.write('Avaliações')
+
+
+if __name__ == '__main__':
+    main()
