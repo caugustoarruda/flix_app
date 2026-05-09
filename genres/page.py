ca4eaa68b374d53e3b38ed9d791dbb1dc8fd1f07
Carlos Arruda <caugustogarruda@gmail.com>
2026-05-09 18:12:24 -0300

    Show genres and update requeriments

--- genres/page.py
+++ genres/page.py
@@ -1,25 +1,30 @@
 import streamlit as st
+import pandas as pd
+from st_aggrid import AgGrid
+import requests
 
 
 genres = [
     {
         'id': 1,
-        'name': 'Ação'
-    },
-    {
-        'id': 2,
-        'name': 'Comédia'
-    },
-    {
-        'id': 3,
         'name': 'Drama'
     }
 ]
-
 def show_genres():
     st.write("Page genres")
+    # token = requests.post(url='http://127.0.0.1:8000/api/v1/authentication/token/', json={
+    #     "username": "carruda",
+    #     "password": "Root@2026"
+    # })
+    # access = token.json().get('access')
+    # headers = {
+    #     'Authorization': f'Bearer {access}'
+    # }
+    # genres = requests.get(url='http://127.0.0.1:8000/api/v1/movies/', headers=headers)
+    # print(genres.json())
 
-    st.table(genres)
+
+    AgGrid(pd.DataFrame(genres))
 
     st.title('Cadastrar novo genero:')
     name = st.text_input('Nome do genero')
