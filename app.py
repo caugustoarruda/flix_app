c822b919c760da09b29e51b984707ef2785ca004
Carlos Arruda <caugustogarruda@gmail.com>
2026-05-10 09:46:43 -0300

    Login service part 2

--- app.py
+++ app.py
@@ -3,30 +3,34 @@ from genres.page import show_genres
 from actors.page import show_actors
 from movies.page import show_movies
 from reviews.page import show_reviews
+from login.page import show_login
 
 
 def main():
-    st.title('Flix app')
-    
-    menu_option = st.sidebar.selectbox(
-        'Selecione uma opção: ',
-        ['Início', 'Generos', 'Atores', 'Filmes', 'Avaliações']
-    )
-
-    if menu_option == 'Início':
-        st.write('Inicio')
-
-    elif menu_option == 'Generos':
-        show_genres()
-
-    elif menu_option == 'Atores':
-        show_actors()
-
-    elif menu_option == 'Filmes':
-        show_movies()
-        
+    if 'token' not in st.session_state:
+        show_login()
     else:
-        show_reviews()
+        st.title('Flix app')
+        
+        menu_option = st.sidebar.selectbox(
+            'Selecione uma opção: ',
+            ['Início', 'Generos', 'Atores', 'Filmes', 'Avaliações']
+        )
+
+        if menu_option == 'Início':
+            st.write('Inicio')
+
+        elif menu_option == 'Generos':
+            show_genres()
+
+        elif menu_option == 'Atores':
+            show_actors()
+
+        elif menu_option == 'Filmes':
+            show_movies()
+            
+        else:
+            show_reviews()
 
 
 if __name__ == '__main__':
